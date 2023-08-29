35bfdcbdce7174648069bd638d6d5f123f1a116e
azwerlein <azwerlein@my.wctc.edu>
2023-08-29 08:34:17 -0500

    Toast sound

--- js/client.js
+++ js/client.js
@@ -1,6 +1,12 @@
 $(function () {
+    // preload audio
+    var toast = new Audio('toast.wav');
     $('.code').on('click', function (e) {
         e.preventDefault();
-        $('#liveToast').toast().toast('show');
+        toast.pause();
+        toast.currentTime = 0;
+        // play audio
+        toast.play();
+        $('#liveToast').toast({ autohide: false }).toast('show');
     });
 });
