de5c637d0156de9ebfd426169cd23828abc27975
azwerlein <azwerlein@my.wctc.edu>
2023-08-29 09:56:39 -0500

    The toast now displays the product and code

--- js/client.js
+++ js/client.js
@@ -2,11 +2,16 @@ $(function () {
     // preload audio
     var toast = new Audio('toast.wav');
     $('.code').on('click', function (e) {
+        let name = $(this).data('product');
+        let code = $(this).data('code');
         e.preventDefault();
         toast.pause();
         toast.currentTime = 0;
         // play audio
         toast.play();
+
+        $('#productName').text(name);
+        $('#code').text(code);
         $('#liveToast').toast({ autohide: false }).toast('show');
     });
     // Hide toast on escape
